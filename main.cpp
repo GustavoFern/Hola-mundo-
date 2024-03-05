4e442f68dedf4406e0497cbede554da828e55163
GustavoFern <160290132+GustavoFern@users.noreply.github.com>
2024-03-05 01:48:53 +0000

    version 5

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include <Alimento.hpp>
 using namespace std;
 
@@ -7,23 +8,39 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    list<Dibujo> dibujos;
+
+
     Dibujo dibujo1("./assets/mimi.txt");
     dibujo1.Dibujar();
     cout << endl;
     Dibujo dibujo2("./assets/JM.txt");
     dibujo2.Dibujar();
+    
+    // dibujos.push_front(dibujo1);       ctrl+k soltar k y luego c
+    // dibujos.push_front(dibujo2);
 
-    Mascota m1;
-    Mascota m2;
-    Mascota m3;
+    Mascota m1,m2,m3;
+
+    list<Mascota> mascotas;
+    mascotas.push_back(m1);
+    mascotas.push_back(m2);
+    mascotas.push_back(m3);
 
     Alimento alimento(5);
 
-    m1.Comer(alimento);
-    m2.Comer(alimento);
-    m3.Comer(alimento);
+    for (auto &&mascotaActual : mascotas)//forrrange
+    {
+        mascotaActual.Comer(alimento);
+    }
 
+    for (auto &&mascotaActual : mascotas)
+    {
+        cout << "Mascota " << mascotaActual.LeerEnergia() << endl;
+    }
 
-    cout << "Mascota " << m1.LeerEnergia() << endl;
     return 0;
 }
+/*ctrl sobre la linea con error
+
+*/
